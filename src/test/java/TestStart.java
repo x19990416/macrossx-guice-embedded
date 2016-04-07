4aa2a07221b4250be47da389ef0952d077ba46c7
x19990416 <starseeker.limin@gmail.com>
2016-04-07 09:39:03 +0800

    update

--- src/test/java/TestStart.java
+++ src/test/java/TestStart.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright (C) 2016 X-Forever.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writi ng, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 import java.io.IOException;
 import java.util.EventListener;
 import java.util.List;
@@ -20,21 +36,6 @@ import com.macrossx.embedded.jetty.JettyServer;
 
 import junit.framework.TestCase;
 
-/**
- * Copyright (C) 2016 X-Forever.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writi ng, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
 
 public class TestStart extends TestCase {
 
